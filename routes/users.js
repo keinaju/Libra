a5c83f24fbc7571afba95c04575967fdcda310c7
Jukka Keinänen <jukka.keinane@gmail.com>
2024-04-06 11:24:37 +0300

    V1 release

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/jukka', function (req, res, next) {
-  res.send('Jukka on kova!');
-});
-
-router.get('/ole', function (req, res, next) {
-  res.send('Ollin sivu');
-});
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
-
-router.get('/hetzku', function (req, res, next) {
-  res.send('Hetan sivu');
 });
 
 module.exports = router;
